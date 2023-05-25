10409d120fd37d80ac039644bdda8074c76150bf
kethnal <kaseyvaldez@hotmail.com>
2023-05-24 17:29:00 -0700

    Style footer for tablet

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -26,45 +26,47 @@ export default function Footer({ categories }: { categories: CategoryProps[] }) 
 
   return (
     <footer>
-      <div className="top">
-        <Image
-          priority
-          src="logo.svg"
-          alt=""
-          width={143}
-          height={25}
-        />
-        <ul>
-          <li>
-            <Link href="/" className="nav-item">
-              HOME
-            </Link>
-          </li>
-          {footerItems}
-        </ul>
-      </div>
-      <div className="bottom">
-        <p className="bottom__blurb">
-          Audiophile is an all in one stop to fulfill your audio needs. We&apos;re a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we’re open 7 days a week.
-        </p>
-        <p className="bottom__copywrite">Copyright 2021. All Rights Reserved</p>
-        <ul>
-          <li>
-            <Link href="/" aria-label="Facebook">
-              <Facebook/>
-            </Link>
-          </li>
-          <li>
-            <Link href="/" aria-label="Twitter">
-              <Twitter/>
-            </Link>
-          </li>
-          <li>
-            <Link href="/" aria-label="Instagram">
-              <Instagram/>
-            </Link>
-          </li>
-        </ul>
+      <div className="wrapper">
+        <div className="top">
+          <Image
+            priority
+            src="logo.svg"
+            alt=""
+            width={143}
+            height={25}
+          />
+          <ul>
+            <li>
+              <Link href="/" className="nav-item">
+                HOME
+              </Link>
+            </li>
+            {footerItems}
+          </ul>
+        </div>
+        <div className="bottom">
+          <p className="bottom__blurb">
+            Audiophile is an all in one stop to fulfill your audio needs. We&apos;re a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we’re open 7 days a week.
+          </p>
+          <p className="bottom__copywrite">Copyright 2021. All Rights Reserved</p>
+          <ul>
+            <li>
+              <Link href="/" aria-label="Facebook">
+                <Facebook/>
+              </Link>
+            </li>
+            <li>
+              <Link href="/" aria-label="Twitter">
+                <Twitter/>
+              </Link>
+            </li>
+            <li>
+              <Link href="/" aria-label="Instagram">
+                <Instagram/>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
