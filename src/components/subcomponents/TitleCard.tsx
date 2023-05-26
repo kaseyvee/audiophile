0d4fde8f5564297df55cd9d069e1f57de5938ca0
kethnal <kaseyvaldez@hotmail.com>
2023-05-26 04:32:27 -0700

    Make titlecard component take props

--- src/components/subcomponents/TitleCard.tsx
+++ src/components/subcomponents/TitleCard.tsx
@@ -1,12 +1,53 @@
 import Link from "next/link";
 
-export default function TitleCard(props) {
+interface ProductProps {
+  name: string;
+  description: string;
+  category: string;
+  productId: string;
+}
+
+interface TitleCardProps {
+  product: ProductProps;
+  isNew?: boolean;
+  isH1?: boolean;
+  isFeature?: boolean;
+  textColor: "black" | "white";
+}
+
+export default function TitleCard({
+  product,
+  isNew,
+  isH1,
+  isFeature,
+  textColor,
+}: TitleCardProps) {
   return (
-    <div className="title-card">
-      <span className="new-product-label">NEW PRODUCT</span>
-      <h1 className="feature-header">XX99 MARK II HEADPHONES</h1>
-      <p>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
-      <Link href={`/`} className="button button__orange">SEE PRODUCT</Link>
+    <div className="title-card" style={{ color: textColor }}>
+      {isNew && (
+        <span
+          className="new-product-label"
+          style={{ color: textColor === "black" ? "white" : "#D87D4A" }}
+        >
+          NEW PRODUCT
+        </span>
+      )}
+      {isH1 ? (
+        <h1 className={isFeature ? "feature-header" : "product-header"}>
+          {product.name}
+        </h1>
+      ) : (
+        <h2 className="feature-header">{product.name}</h2>
+      )}
+      <p>{product.description}</p>
+      <Link
+        href={`/${product.category}/${product.productId}`}
+        className={`button button__${
+          textColor === "white" ? "black" : "orange"
+        }`}
+      >
+        SEE PRODUCT
+      </Link>
     </div>
   );
 }
