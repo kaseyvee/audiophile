04e37969f34b703e240d03faf46dd21f2b8d96d8
kethnal <kaseyvaldez@hotmail.com>
2023-05-25 17:34:15 -0700

    Style bottom banner for desktop

--- src/components/BottomBanner.tsx
+++ src/components/BottomBanner.tsx
@@ -12,10 +12,12 @@ function BottomBanner() {
         />
         <img src="shared/mobile/image-best-gear.jpg" alt="" />
       </picture>
-      <h2 className="blurb-header">BRINGING YOU THE <span>BEST</span> AUDIO GEAR</h2>
-      <p>
-        Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.
-      </p>
+      <div className="bottom-banner__words">
+        <h2 className="blurb-header">BRINGING YOU THE <span>BEST</span> AUDIO GEAR</h2>
+        <p>
+          Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.
+        </p>
+      </div>
     </div>
   );
 }
