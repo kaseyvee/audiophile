56ee03174b1b617b541b2e3dea7f8e25d6a7f3de
kethnal <kaseyvaldez@hotmail.com>
2023-05-26 04:06:04 -0700

    Add hardcoded placeholders for titlecard and style

--- src/components/subcomponents/TitleCard.tsx
+++ src/components/subcomponents/TitleCard.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function TitleCard(props) {
   return (
-    <div>
+    <div className="title-card">
       <span className="new-product-label">NEW PRODUCT</span>
       <h1 className="feature-header">XX99 MARK II HEADPHONES</h1>
       <p>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
