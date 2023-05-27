9ec0b26f7dfb8e11c51651db9903d0b5148a62a8
kethnal <kaseyvaldez@hotmail.com>
2023-05-27 01:48:59 -0700

    Style category header for all pages

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -5,13 +5,16 @@ import CategoryList from "@/components/subcomponents/CategoryList";
 import { getCategories } from "@/fetching/getCategories";
 
 export default async function Category({ params }: { params: { category: string }}) {
+  const currentCategory = params.category;
   const categories = await getCategories();
   const products = await getProductsByCategory(params.category);
   console.log(products, "PRODUCTSSSS")
 
   return (
     <main className="category page">
-      
+      <header>
+        <h1 className="category-header">{currentCategory.toUpperCase()}</h1>
+      </header>
       <div className="bottom">
         <div className="wrapper">
           <CategoryList categories={categories}/>
